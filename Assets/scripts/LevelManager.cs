50eee99e814e05294ed3b1f20af3af244016a08f
gvdlx13 <gamalax2@gmail.com>
2016-07-16 20:12:22 -0600

    first!!

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public float autoLoadNextLevelAfter; 
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextLevel()
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Requested");
+        Application.Quit();
+    }
+
+
+    
+
+}
